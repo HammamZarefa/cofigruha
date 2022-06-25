0bb512b2cf30b545566d2da5ab68e422e5f20073
HammamZarefa <HammamZarefa@gmail.com>
2022-06-25 18:28:42 +0300

    curso codego

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Anapat - Admin Login</title>
+    <title>Cofigruha - Admin Login</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{asset('admin/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
